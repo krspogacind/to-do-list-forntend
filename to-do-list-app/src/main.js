f99726c4b6d947906b21dc0c998e0f27f3cf625e
Darko Krspogacin <darko.krspogacin@vivifyideas.com>
2020-12-22 17:57:39 +0100

    implement login

--- to-do-list-app/src/main.js
+++ to-do-list-app/src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "./index.css";
-import router from "./router.js";
+import router from "./router";
 import "./axios";
+import store from "./vuex";
+import Toast from "vue3-toast-single";
+import "vue3-toast-single/dist/toast.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(Toast, { verticalPosition: "bottom", duration: 1500 })
+  .mount("#app");
--- to-do-list-app/src/router.js
+++ to-do-list-app/src/router.js
@@ -1,6 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "./components/Home.vue";
 import Registration from "./components/Registration.vue";
+import Login from "./components/Login.vue";
 
 const history = createWebHistory();
 const router = createRouter({
@@ -8,6 +9,7 @@ const router = createRouter({
   routes: [
     { path: "/", component: Home },
     { path: "/registration", component: Registration },
+    { path: "/login", component: Login },
   ],
 });
 
